972741753d92571c94a5bbcb22a2a2e3ab631db3
Яхина Наталья Андреевна <  @vvrz.local>
2022-06-24 14:17:48 +0300

    00

--- Task_00/Program.cs
+++ Task_00/Program.cs
@@ -1,2 +1,31 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Универсальная программа введения в степень числа
+// Console.Write ("введите целое число: ");
+// int num = Convert.ToInt32 (Console.ReadLine());
+// double square = Math.Pow (num, 5);
+// Console.Write ($"Квадрат числа {num} - {square}");
+
+
+// Напишите программу, которая на вход принимает число и выдает его квадрат (число умножает на само себя)
+// Например: 4 -> 16, 3 -> 9, -7 -> 49
+
+
+Console.Write ("введите целое число: ");
+int num = Convert.ToInt32 (Console.ReadLine());
+int square = num*num;
+Console.Write ($"Квадрат числа {num} - {square}");
+
+// if (num > 5)
+// {
+//     Console.WriteLine("ОК!");
+// }
+// else Console.WriteLine("Ошибка!");
+// while (num > 5)
+// {
+
+// }
+// double d = 5.55;
+// int i = (int)d;
+// Console.WriteLine(i);
+// Console.Write ($"Квадрат числа {num} - {square}");
+
+
