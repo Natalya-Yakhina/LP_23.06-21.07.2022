447091848d4eacc7e5b2450c023fb50a7e5ad4e3
Natalya <ivanechka93@mail.ru>
2022-06-27 20:38:28 +0300

    033

--- Task_03/Program.cs
+++ Task_03/Program.cs
@@ -9,6 +9,11 @@ Console.WriteLine($"Случайное число из отрезка 10-99 => {
 
 int firstDigit = rndNum / 10;
 int secondDigit = rndNum % 10; 
-if (firstDigit > secondDigit) Console.WriteLine($"Наибольшая цифра числа: {rndNum} => {firstDigit}");
-else if (firstDigit < secondDigit) Console.WriteLine($"Наибольшая цифра числа: {rndNum} => {secondDigit}");
+
+int maxDigit = firstDigit > secondDigit ? firstDigit : secondDigit; //тернарный оператор может возвращать значение и записвать в переменную (? - тогда if, : если else)
+// int maxDigit = firstDigit;
+// if(secondDigit > maxDigit) maxDigit = secondDigit;
+// else if (secondDigit == maxDigit) maxDigit = 0;
+
+if(maxDigit > 0) Console.WriteLine($"Наибольшая цифра числа: {rndNum} => {maxDigit}");
 else Console.WriteLine($"Цифры числа {rndNum} равны");
