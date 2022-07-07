e891e7ad9328e8528d4b49c249301768be9d2d74
Natalya <ivanechka93@mail.ru>
2022-07-07 22:26:12 +0300

    037

---
+++ Task_037/Program.cs
@@ -0,0 +1,30 @@
+﻿// Задача 37: Найдите произведение пар чисел в одномерном массиве.
+// Парой считаем первый и последний элемент, второй и предпоследний
+// и т.д. Результат запишите в новом массиве.
+// [1 2 3 4 5] -> 5 8 3
+// [6 7 3 6] -> 36 21
+
+Console.Clear();
+
+int n = 3;
+int[] arr = new int[n];
+Random rnd = new Random();
+
+for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = rnd.Next(0, 10);
+        Console.Write(arr[i] + " ");
+    }
+Console.WriteLine();
+
+int j = arr.Length - 1;
+    for (int i = 0; i < (arr.Length + 1) / 2; i++)
+    {
+    Console.Write(arr[i] * arr[j] + " ");
+    j--;
+    }
+
+if (n % 2 > 0)
+{
+    Console.Write(arr[arr.Length / 2 + 1]);
+}
