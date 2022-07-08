64bf756007439405e6124da534fc4f07146656b8
Наталья Яхина <ivanechka93@mail.ru>
2022-07-08 08:41:36 +0300

    035

--- Task_035/Program.cs
+++ Task_035/Program.cs
@@ -28,3 +28,55 @@ count = count + 1;
 Console.WriteLine();
 Console.Write("Массив совержит следующее количество элементов из отрезка [10,99] - ");
 Console.Write(count);
+
+
+// Console.Write("Введите длинну массива: ");
+// int sizeArr = int.Parse(Console.ReadLine());
+
+// Console.Write("Введите минимальное значение элементов массива: ");
+// int minimal = int.Parse(Console.ReadLine());
+
+// Console.Write("Введите максимальное значение элементов массива: ");
+// int maximum = int.Parse(Console.ReadLine());
+
+// int[] CreateArrayRndInt(int size, int min, int max)
+// {
+//     int[] arr = new int[size];
+//     Random rnd = new Random();
+
+//     for (int i = 0; i < size; i++)
+//     {
+//         arr[i] = rnd.Next(min, max + 1);
+//     }
+//     return arr;
+// }
+
+// int FindNums (int[] arr)
+// {
+// int count = 0;
+//     for (int i = 0; i < arr.Length; i++)
+//     {
+//         if (arr[i] >= 10 && arr[i] <= 99)
+//         {
+//             count += 1;
+//         }
+//     }
+//     return count;
+// }
+
+// void PrintArray (int[] arr)
+// {
+//     for (int i = 0; i < arr.Length; i++)
+//     {
+//         if (i==0) Console.Write("Массив [");
+//         if (i < arr.Length - 1) Console.Write(arr[i] + ",");
+//         else Console.Write(arr[i] + "]");
+        
+//     }
+// }
+
+// int[] array = CreateArrayRndInt (sizeArr, minimal, maximum);
+// int count = FindNums(array);
+// PrintArray(array);
+// Console.WriteLine();
+// Console.WriteLine($"Количество элементов массива, значения которых лежат в отрезке [10,99]: {count}");
