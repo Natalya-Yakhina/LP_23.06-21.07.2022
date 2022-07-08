02d2a3ed875a761bc912d148c99b901868e35add
Наталья Яхина <ivanechka93@mail.ru>
2022-07-08 10:31:46 +0300

    032

--- Task_032/Program.cs
+++ Task_032/Program.cs
@@ -2,26 +2,26 @@
 // положительные элементы замените на соответствующие отрицательные и наоборот
 // [-4, -8, 8, 2] --> [4, 8, -8, -2]
 
-int[] array1 = {-4, 3, -9, 2};
+int[] array1 = { -4, 3, -9, 2 };
 
 int[] Integer(int[] arr)
 {
-for (int i = 0; i < arr.Length; i++)
-{
-arr[i] = arr[i] * -1;
-// arr[i] = -arr[i];
-}
-return arr;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = arr[i] * -1;
+        // arr[i] = -arr[i];
+    }
+    return arr;
 }
 
 void PrintArray(int[] arr)
 {
-for (int i = 0; i < arr.Length; i++)
-{
-if (i == 0) Console.Write("[");
-if (i < arr.Length - 1) Console.Write(arr[i] + ",");
-else Console.Write(arr[i] + "]");
-}
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (i == 0) Console.Write("[");
+        if (i < arr.Length - 1) Console.Write(arr[i] + ",");
+        else Console.Write(arr[i] + "]");
+    }
 }
 
 int[] array2 = Integer(array1);
