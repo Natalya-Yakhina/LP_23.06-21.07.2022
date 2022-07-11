d5bffe872f3212cda40200bf784c1310111918f7
Natalya <ivanechka93@mail.ru>
2022-07-11 21:01:38 +0300

    11.07

---
+++ Task_040/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задача 40. Напишите программу, которая принимает на вход три числа и проверяет,
+// может ли существовать треугольник со сторонами такой длины
+// А < B + C
+
+Console.WriteLine("Введите первую сторону: ");
+int numA = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите вторую сторону: ");
+int numB = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите третью сторону: ");
+int numC = Convert.ToInt32(Console.ReadLine());
+
+void Triagle(int a, int b, int c)
+{
+    if(a < b + c && b < a +c && c < a + b)
+    Console.WriteLine("Треугольник существует");
+    else 
+    {
+    Console.WriteLine ("Треугольник не существует");
+    }
+}
+Triagle(numA, numB, numC);
---
+++ Task_042/Program.cs
@@ -0,0 +1,37 @@
+﻿// Задача 42. Напишите программу, которая будет преобразовывать десятичное число в двоичное.
+// 3 ---> 11
+// 2 ---> 10
+
+// void Base10to2(int num)
+// {
+//     string number =  " ";
+//     while(num > 0)
+//     {
+//         number = num % 2 + number;
+//         num /= 2;
+//     }
+//     Console.WriteLine(number);
+// }
+Console.Write("Введите числo: ");
+int num = Convert.ToInt32(Console.ReadLine());
+Base10to2(num);
+
+
+void Base10to2(int num1)
+{
+    int a = 0;
+    int i = 0;
+
+    int[] b = new int[10];
+    while (num1 >= 1)
+    {
+        a = num1 % 2;
+        b[i] = a;
+        i++;
+
+        num1 = num1 / 2;
+
+        Console.Write(a);
+    }
+}
+Base10to2(num);
