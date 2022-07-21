35d2e808b8891472201f293c122e2b13000fd1ab
Natalya <ivanechka93@mail.ru>
2022-07-21 22:02:49 +0300

    21072022

---
+++ Task_067/Program.cs
@@ -0,0 +1,31 @@
+﻿// Задача 67: Напишите программу, которая будет принимать на вход число и
+// возвращать сумму его цифр.
+// 453 -> 12
+// 45 -> 9
+
+Console.Write("Задайте число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+void SumNumber(int num, int sum)
+{
+    int ostatok = num % 10;
+    sum = sum + ostatok;
+    if(ostatok == 0)
+    {
+        Console.Write($"Сумма цифр числа --> {sum}");
+        return;
+    }
+    num /= 10;
+    SumNumber(num, sum);
+}
+SumNumber(num, 0);
+
+
+// int number = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine(RecursionSum(number));
+
+// int RecursionSum(int n)
+// {
+//     if (n == 0) return 0;
+//     return n % 10 + RecursionSum(n / 10);
+// }
---
+++ Task_069/Program.cs
@@ -0,0 +1,41 @@
+﻿// Задача 69: Напишите программу, которая на вход принимает два числа A и B, и
+// возводит число А в целую степень B с помощью рекурсии.
+// A = 3; B = 5 -> 243 (3⁵)
+// A = 2; B = 3 -> 8
+
+Console.Clear();
+Console.WriteLine("Задайте число для возведения в степень: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Задайте степень для числа: ");
+int m = Convert.ToInt32(Console.ReadLine());
+
+
+void PowerNumber(int num, int power, int memory)
+{   
+    if(power > 1)
+    {
+        num = num * memory;
+        PowerNumber(num, power - 1, memory);
+        return;
+    } 
+    else
+    {
+        Console.Write($"число {n} в степени {m} --> {num} ");
+    }
+}
+PowerNumber(n, m, n);
+
+// Console.WriteLine("Задайте число A: ");
+// int A = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Задайте число B: ");
+// int B = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine(PowerNumber(A, B));
+
+
+// int PowerNumber(int number, int power)
+// {
+//     if (power == 1) return number;
+//     return number * PowerNumber(number, --power);
+//     Console.Write($"{power} ");
+//     PowerNumber(A, B);
+// }
